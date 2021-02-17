545c4619f6984f9ae30696e7de364632a3d064e5
Siddhant Aggarwal <siddhant197@gmail.com>
2021-02-17 15:02:25 +0530

    Adding Initial possibleMoves function to check valid parameters

---
+++ index.js
@@ -0,0 +1,26 @@
+
+//Function to check all possible moves piece can move from it's current position
+//Parameters Accepted is one of the Pieces from (King, Queen, Bishop, Horse, Rook, Pawn)
+//And It's Current position which from range A1,A2,A3,A4,A5,A6,A7,A8, similarly for B1...B8, C1...C8, D1...D8,
+//E1...E8, F1...F8, G1...G8, H1...H8
+
+function possibleMoves(piece, pos){
+    //Two Arrays to handle Horizontal moves & Vertical Moves
+    const xMoves = ['A','B','C','D','E','F','G','H'];
+    const yMoves = ['1','2','3','4','5','6','7','8'];
+    const validPiece = ['King','Queen','Bishop','Horse', 'Rook', 'Pawn'];
+
+    //Checking if the input given are either of valid pieces
+    if(validPiece.indexOf(piece) < 0){
+        console.log("Invalid Input");
+        return false;
+    }
+
+    //Checking if the input given are either of valid position
+    if(!piece || pos.length!=2 || !xMoves.includes(pos[0]) || !yMoves.includes(pos[1])){
+        console.log("Invalid Input");
+        return false;
+    }
+}
+
+module.exports = possibleMoves;
---
+++ test.js
@@ -0,0 +1,6 @@
+const possibleMoves = require('./index');
+
+test('chess possibleMoves function is defined', () => {
+  expect(possibleMoves).toBeDefined();
+});
+
